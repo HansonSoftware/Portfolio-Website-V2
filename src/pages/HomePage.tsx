3ee42499e25a4274b99b595a75f2cdbde6f376f1
Hayden Hanson <hansonhayd3n@gmail.com>
2022-12-20 14:15:54 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePage() {
     return (
         <div>
-            Hello World! I'm Hayden.
+            Hello World! I'm Hayden. 
         </div>
     )
 }

ca42c899a559665ad8e95c30c067f9463b149450
Hayden Hanson <hansonhayd3n@gmail.com>
2022-12-30 10:16:15 -0500

    Finish WritingPage Blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { database } from '../firebase'
 
 export default function Blog() {
+    // interface Post {
+    //     id: string, 
+    //     title:string, 
+    //     author:string, 
+    //     date:string, 
+    //     content:string
+    // };
+    
+    const [posts, setPosts] = useState([{}]);
+    const postsCollection = collection(database, "posts");
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollection);
+            setPosts(data.docs.map((doc => ({ ...doc.data(), id: doc.id}))))
+        }
+        getPosts();
+    },[]);
+
     return (
         <div className="pt-40 grid place-content-center gap-16 lg:px-20 px-10">
             <div className="flex flex-row gap-4">
@@ -14,26 +35,37 @@ export default function Blog() {
                             <h2 className="card-title">How does this work?</h2> 
                             <p>Embedded into this website is a blog app that I built to share more content, to those who are interested.</p>
                             <br/>
-                            <p>This blog app is built with Firebase and TypeScript. My posts are retirieved from the database and formatted here.</p>
+                            <p>This blog app is built with Firebase and JavaScript. My posts are retirieved from the database and formatted here.</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPosts />
+                {posts.map((post) =>
+                    <BlogPost key={post.id} title={post.title} author={post.author} date={post.date} content={post.content} />
+                )}
         </div>
     )
 }
 
-const BlogPosts = () => {
+// interface BlogPostProps {
+//     key:string,
+//     title:string,
+//     author:string,
+//     date:string, 
+//     content:string,
+// }
+
+const BlogPost = ({ title, author, date, content }) => {
+
     return (
         <div className="bg-base-200 flex flex-col flex-grow-1 w-full items-center border-2 border-base-300 bg-opacity-80 backdrop-blur-sm rounded-3xl">
             <div className="flex flex-col p-4 items-start gap-6 w-full" style={{width: "20rem"}}>
                 <div className="flex flex-col">
-                    <h1 className="text-2xl">Example Post</h1>
-                    <h2 className="text-lg">Hayden | 12/28/22</h2>
+                    <h1 className="text-2xl">{title}</h1>
+                    <h2 className="text-lg">{author} | {date}</h2>
                 </div>
                 <div className="w-full p-2 border border-base-100 rounded-xl">
-                    <p className="">This post came from firebase! Woot woot</p>
+                    <p className="">{content}</p>
                 </div>
             </div>
         </div>

084aac11983c50afddd7d967eecc98d9ba774177
Hayden Hanson <hansonhayd3n@gmail.com>
2023-03-10 16:05:55 -0500

    Update Contact Page

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
+import { AiFillGithub } from 'react-icons/ai'
+import { BsFileEarmarkPerson } from 'react-icons/bs'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { AiOutlineCodepenCircle } from 'react-icons/ai'
+import { AiFillMail } from 'react-icons/ai'
+import { SiFrontendmentor } from 'react-icons/si'
 
 export default function ContactForm() {
     return (
         <div className="pt-32">
-            <div className="lg:px-20 px-10">
-                <h1 className="pb-8 lg:text-8xl md:text-7xl text-6xl">Contact</h1>
-                {/* <div className="grid place-content-center pb-32">
-                    <div className="flex flex-col gap-2 max-w-2xl">
-                        <h1 className="pb-4 lg:text-5xl md:text-4xl text-3xl">Connect With Me:</h1>
-                            <p>GitHub: ___</p>
+            <div className="lg:px-20 px-10 grid place-content-center animate-slideup">
+                <h1 className="pb-8 lg:text-8xl md:text-7xl text-5xl">Get in touch.</h1>
+                <div className="grid place-content-center">
+                    <div className="flex flex-col gap-2 pb-32">
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><AiFillGithub style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">Github: <a className="underline" href="/" about="__blank">HansonSoftware</a></h1>
+                        </div>
+
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><AiFillLinkedin style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">LinkedIn: <a className="underline" href="/" about="__blank">HansonHayden</a></h1>
+                        </div>
+
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><AiOutlineCodepenCircle style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">CodePen: <a className="underline" href="/" about="__blank">HansonHayden</a></h1>
+                        </div>
+
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><SiFrontendmentor style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">Frontend Mentor: <a className="underline" href="/" about="__blank">Link</a></h1>
+                        </div>
+
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><AiFillMail style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">Email: <a className="underline" href="/" about="__blank">HaydenHansonDev</a></h1>
+                        </div>
+
+                        <div className="flex flex-row gap-4">
+                            <h1 className="text-primary"><BsFileEarmarkPerson style={{width: "30", height: "30"}}/></h1>
+                            <h1 className="lg:text-3xl md:text-2xl text-xl">Resume: <a className="underline" href="/" about="__blank">View</a> | <a className="underline" href="/" about="__blank">Download</a></h1>
+                        </div>
                     </div>
-                </div> */}
+                </div>
                 <div className="grid place-content-center">
                     {/* Form Container */}
                     <div className="flex flex-col gap-2 max-w-2xl">
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Contact from '../components/Contact'
 import ContactForm from '../components/ContactForm'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 export default function ContactPage() {
+    const { pathname } = useLocation();
+
+    /* Now why is this here you might ask?
+     * React Router has this annoying tendency of remembering your scroll position.
+     * So, when a ProjectTile brings you here, you're in the middle of the page!
+     * To fix that I use useEffect and scroll the user to the top of the page.
+     */
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
     return (
         <div>
             <Navbar />
